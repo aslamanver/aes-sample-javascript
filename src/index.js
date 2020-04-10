f9def4056134f2adce3e392dfcd6a49a7c2a6067
Aslam Anver <slam.anver9@gmail.com>
2020-04-10 09:45:20 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const CryptoJS = require("crypto-js");
 
-var encrypted = CryptoJS.AES.encrypt('{"test":1}', "175345a789abcdef", {
-  mode: CryptoJS.mode.ECB,
-  padding: CryptoJS.pad.NoPadding
+let data = '{"test":1}'
+let key = 'NdRgUkXp2s5v8y/A'
+
+console.log('\n---------------- Encryption ----------------\n')
+
+var encrypted = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
 });
 
 console.log(encrypted.toString());
 
-// /sag5h5swQKND6ZmdlfuIg==
+console.log('\n---------------- Decryption ----------------\n')
+
+var decrypted = CryptoJS.AES.decrypt(encrypted.toString(), CryptoJS.enc.Utf8.parse(key), {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
+})
+
+console.log(decrypted.toString(CryptoJS.enc.Utf8) + "\n")
